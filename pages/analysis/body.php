e949040eae680ad79aa290138141d8da8ec81fb6
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-11-05 23:19:08 +0200

    periodic table

--- pages/analysis/body.php
+++ pages/analysis/body.php
@@ -7,8 +7,26 @@
 </p>
 
 <h2 id="intro">Ievads</h2>
+<style>
+	#periodic-table {
+		width: 640px;
+		height: 339px;
+		position: relative;
+	}
+</style>
+
 <p>
 	<i>ievads...</i>
+</p>
+<p>
+	<?php
+		$periodic_table_path = $root_path . "/resources/periodic_table.svg";
+		$periodic_table_style_id = "periodic-table";
+		$alternative_text = "Periodic table missing";
+		echo "<img src='" . $periodic_table_path . "' "
+			. "alt='" . $alternative_text . "' "
+			. "id='" . $periodic_table_style_id . "'>";
+	?>
 </p>
 
 <h2 id="iron">Dzelzs</h2>
