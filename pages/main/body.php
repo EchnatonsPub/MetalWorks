1cd2ea8fffca1fb9d6550b7f92c2587babdfe157
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-11-05 06:32:23 +0200

    page 2

--- pages/main/body.php
+++ pages/main/body.php
@@ -1,3 +1,4 @@
 <h1>TOC</h1>
 <a href="pages/page1/">Page 1</a><br>
-<a href="pages/sample/">sample page</a><br>
+<a href="pages/page2/">Page 2</a><br>
+<!--<a href="pages/sample/">sample page</a><br>-->
---
+++ pages/page2/body.php
@@ -0,0 +1,9 @@
+<h1>Page 2</h1>
+
+<p>
+	Page 2.
+</p>
+
+<?php
+	echo "<a href=" . $root_path . ">Back to main page</a>";
+?>
