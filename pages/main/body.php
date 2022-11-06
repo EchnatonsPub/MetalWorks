833d4ee4f7dedba28d82d089ce7d7de1fe1d8f54
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-11-06 06:56:08 +0200

    elemental data stub

--- pages/main/body.php
+++ pages/main/body.php
@@ -2,10 +2,10 @@
 
 <h2>TOC</h2>
 <p>
-	<a href="pages/cenas/">Cenas</a><br>
-	<a href="pages/melt/">Kausēšana</a><br>
-	<a href="pages/analysis/">Analīze</a><br>
-	<a href="pages/page1/">Page 1</a><br>
-	<a href="pages/page2/">Page 2</a><br>
+	<a href="pages/cenas">Cenas</a><br>
+	<a href="pages/melt">Kausēšana</a><br>
+	<a href="pages/analysis">Analīze</a><br>
+	<a href="pages/page1">Page 1</a><br>
+	<a href="pages/page2">Page 2</a><br>
 	<!--<a href="pages/sample/">sample page</a><br>-->
 </p>
--- pages/melt/body.php
+++ pages/melt/body.php
@@ -1,15 +1,48 @@
 <h1>Kausēšana</h1>
+<?php
+	include($root_path . "/resources/element-info.php");
+	
+	function make_simple_link($tag, $name) {
+		echo "<a href='#" . $tag . "'>" . $name . "</a><br>";
+	}
+	
+	function make_link($element_number) {
+		global $elements;
+		$tag = $elements->get($element_number)->tag;
+		$name = $elements->get($element_number)->name;
+		make_simple_link($tag, $name);
+	}
+	
+	function make_simple_heading($tag, $name) {
+		echo "<h2 id='" . $tag . "'>" . $name . "</h2>";
+	}
+	
+	function make_heading($element_number) {
+		global $elements;
+		$tag = $elements->get($element_number)->tag;
+		$name = $elements->get($element_number)->name;
+		make_simple_heading($tag, $name);
+	}
+?>
+
+<h2>TOC</h2>
 <p>
-	<a href="#aaa">aaa</a><br>
-	<a href="#bbb">bbb</a>
+	<?php
+		make_link(13);
+		make_simple_link("bbb", "bbb");
+	?>
 </p>
 
-<h2 id="aaa">aaa</h2>
+<?php
+	make_heading(13);
+?>
 <p>
-	<i>coming soon...</i>
+	Alumīnijs kūst pie 660.32 °C.
 </p>
 
-<h2 id="bbb">bbb</h2>
+<?php
+	make_simple_heading("bbb", "bbb");
+?>
 <p>
 	<i>coming soon...</i>
 </p>
